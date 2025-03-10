025a78a28fe47acd737ccc048348e6e04d0dcbed
dani2386 <danielfnunes04@gmail.com>
2025-03-10 00:14:42 +0000

    Implicit scheme and RK4 + optimization

--- src/WaveEquationSolver.py
+++ src/WaveEquationSolver.py
@@ -1,70 +1,55 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 
 
 class WaveEquationSolver:
-    def __init__(self, n, m, delta_t, delta_x):
-        self.N = n
-        self.M = m
-        self.delta_t = delta_t
-        self.delta_x = delta_x
-        self.courant_param = self.delta_t / self.delta_x
-        self.grid = np.empty([self.N + 1, self.M + 1])
+    def __init__(self, n, m, delta_x, delta_t):
+        self._N = n
+        self._M = m
+        self._delta_x = delta_x
+        self._delta_t = delta_t
+        self._courant_param = delta_t / delta_x
+        self._grid = np.zeros([self._N + 1, self._M + 1, 2])
 
-    def second_centered_spatial_derivative(self, n):
-        return self.grid[n, 2:] - 2 * self.grid[n, 1:-1] + self.grid[n, :-2]
-
-    def second_centered_time_derivative(self, n):
-        return self.grid[n + 1, 1:-1] - 2 * self.grid[n, 1:-1] + self.grid[n - 1, 1:-1]
+    @staticmethod
+    def _second_spatial_derivative(arr):
+        return np.concatenate(([0], arr[:-2] - 2 * arr[1:-1] + arr[2:], [0]))
 
     def spatial_domain(self):
-        return np.linspace(0, self.M * self.delta_x, self.M + 1)
+        return np.linspace(0, self._M * self._delta_x, self._M + 1)
 
-    def time_domain(self):
-        return np.linspace(0, self.N * self.delta_t, self.N + 1)
+    def solve_fd(self, f, g, theta):
+        self._grid[0, :, 0] = f(self.spatial_domain())
+        self._grid[1, :, 0] = (self._grid[0, :, 0] + self._delta_t * g(self.spatial_domain())
+                               + pow(self._courant_param, 2) / 2 * self._second_spatial_derivative(self._grid[0, :, 0]))
 
-    def solve_explicit(self, f, g):
-        # boundary conditions
-        self.grid[:, 0] = self.grid[:, self.M] = 0  # Dirichlet boundary condition (fixed ends)
+        a = (np.diag(2 * (1 / pow(self._courant_param, 2) + theta) * np.ones(self._M + 1), k=0)
+             + np.diag(-theta * np.ones(self._M), k=1) + np.diag(-theta * np.ones(self._M), k=-1))
+        b = np.diag(-2 * np.ones(self._M + 1), k=0) + np.diag(np.ones(self._M), k=1) + np.diag(np.ones(self._M), k=-1)
+        c = 2 * (np.diag(np.ones(self._M + 1), k=0) + np.linalg.inv(a) @ b)
 
-        # initial conditions
-        self.grid[0, 1:-1] = f(self.spatial_domain()[1:-1])
-        self.grid[1, 1:-1] = (self.grid[0, 1:-1] + self.delta_t * g(self.spatial_domain()[1:-1])
-                              + self.courant_param ** 2 / 2 * self.second_centered_spatial_derivative(0))
+        for n in range(1, self._N):
+            self._grid[n + 1, :, 0] = c @ self._grid[n, :, 0] - self._grid[n - 1, : 0]
 
-        # solver (explicit)
-        for n in range(1, self.N):
-            self.grid[n + 1, 1:-1] = (2 * self.grid[n, 1:-1] - self.grid[n - 1, 1:-1]
-                                      + self.courant_param ** 2 * self.second_centered_spatial_derivative(n))
+    def solve_rk4(self, f, g):
+        self._grid[0] = np.column_stack((f(self.spatial_domain()), g(self.spatial_domain())))
+
+        _f = [lambda x: x, lambda x: 1 / pow(self._delta_x, 2) * self._second_spatial_derivative(x)]
+
+        for n in range(self._N):
+            k1_1 = _f[0](self._grid[n, :, 1])
+            k1_2 = _f[1](self._grid[n, :, 0])
+
+            k2_1 = _f[0](self._grid[n, :, 1] + self._delta_t / 2 * k1_2)
+            k2_2 = _f[1](self._grid[n, :, 0] + self._delta_t / 2 * k1_1)
+
+            k3_1 = _f[0](self._grid[n, :, 1] + self._delta_t / 2 * k2_2)
+            k3_2 = _f[1](self._grid[n, :, 0] + self._delta_t / 2 * k2_1)
+
+            k4_1 = _f[0](self._grid[n, :, 1] + self._delta_t * k3_2)
+            k4_2 = _f[1](self._grid[n, :, 0] + self._delta_t * k3_1)
+
+            self._grid[n + 1, :, 0] = self._grid[n, :, 0] + self._delta_t / 6 * (k1_1 + 2 * k2_1 + 2 * k3_1 + k4_1)
+            self._grid[n + 1, :, 1] = self._grid[n, :, 1] + self._delta_t / 6 * (k1_2 + 2 * k2_2 + 2 * k3_2 + k4_2)
 
     def solution_n(self, n):
-        return self.grid[n, :]
-
-    def solution_m(self, m):
-        return self.grid[:, m]
-
-    def solution_mn(self, n, m):
-        return self.grid[n, m]
-
-    def draw_n(self, n):
-        plt.plot(self.spatial_domain(), self.solution_n(n))
-        plt.ylim(1.2 * -np.max(self.grid), 1.2 * np.max(self.grid))
-        plt.show()
-
-    def draw_m(self, m):
-        plt.plot(self.time_domain(), self.solution_m(m))
-        plt.show()
-
-    def animate(self):
-        fig, ax = plt.subplots()
-        ax.set_ylim(1.2 * -np.max(self.grid), 1.2 * np.max(self.grid))
-        line, = ax.plot(self.spatial_domain(), self.solution_n(0))
-
-        def update(frame):
-            line.set_ydata(self.solution_n(frame))
-            return line,
-
-        anim = animation.FuncAnimation(fig, update, frames=self.N, blit=True)
-        writer = animation.FFMpegWriter(fps=15, metadata=dict(artist='Me'), bitrate=1800)
-        anim.save("animation.mp4", writer=writer)
+        return self._grid[n, :, 0]
